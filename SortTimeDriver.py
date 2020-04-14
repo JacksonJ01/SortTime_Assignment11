7887eb894963db09e9f388ed985cc1fcb4e5a2da
JacksonJ01 <jacksonj@hartwick.edu>
2020-04-14 05:35:20 -0400

    QUICK SORT SORTED 100,000 ITEMS IN .4 SECONDS!!!! BUBBLE SORT TOOK 6 MINUTES FOR 50,000
    That is so crazy

--- SortTimeDriver.py
+++ SortTimeDriver.py
@@ -3,14 +3,15 @@ from time import time, sleep
 from random import randint
 numbers = []
 
-input("*PRESS ENTER*")
+input("* click here and PRESS ENTER*".upper())
 sleep(1)
 
 print("\nGreetings, looks like you enjoyed the last program I showed you."
       "\nWell, guess what! "
       "\nYou're in luck, because I have another one just like it."
-      "\nToday we are going to calculate the time it takes the chosen sort method to complete a sort")
-# sleep(2.5)
+      "\nToday we are going to calculate the time it takes the chosen sort method to complete a sort"
+      "\nBut keep this in mind: Computers are very fast, so sorting a menial 20 numbers won't take long at all")
+sleep(2.5)
 
 start = input("\nLike last time, if you would like to see the program, you must type 'start'."
               "\n>>>").title()
@@ -20,7 +21,7 @@ while start != "Start":
     start = input("\nType 'start' please"
                   "\n>>>").title()
     fails += 1
-    # sleep(.25)
+    sleep(.25)
 
 # For added sass
 if fails >= 3:
@@ -30,7 +31,7 @@ print("Alight, let's get this show on the road.\n\n")
 
 menu = "Hello World"
 while menu != "EXIT":
-    # sleep(1.5)
+    sleep(1.5)
     print("_" * 100)
     print("\n_Press The Number Next To Your Desired Choice_")
 
@@ -57,7 +58,7 @@ while menu != "EXIT":
         print("\nThanks for playing")
         quit()
 
-    # sleep(.5)
+    sleep(.5)
     # The user can choose between 'preset' values, or they can enter their own
     menu2 = input("\nHow many numbers would you like to see this method sort?"
                   "\n1. 20,000 numbers"
@@ -89,14 +90,15 @@ while menu != "EXIT":
             sort = input("\nHow many values?"
                          "\n>>>")
 
-            while sort == "HW":
+            while sort != "HW":
                 # checks if they entered a number
                 try:
                     menu2 = int(sort)
+                    break
                 except ValueError:
                     sort = input("\nHow many values would you like to see this method sort?")
 
-        # sleep(1)
+        sleep(1)
         # Appends the values to the list
         for number in range(0, menu2):
             numbers.append(randint(1, 100000))
@@ -110,24 +112,25 @@ while menu != "EXIT":
             # If they want to see 100 numbers, it's not that bad, so I have this text for longer lists
             if menu2 >= 3000:
                 print("Brace yourself:")
-            print("Press Enter when you're done.")
-            # sleep(1.5)
+            print(" Click AND Press Enter when you're done.")
+            sleep(1)
             print(numbers)
             # I don't want the code to just continue while the user looks at the values,
             # so they have to press enter when they're done
-            input("press enter to continue".upper())
+            input("Click here and press enter to continue".upper())
 
-        print("Alright, I'm running it:")
-        # sleep(.5)
+        sleep(.5)
+        print("Alright, I'm running it in:\n")
         countdown = 3
         for down in range(0, 3):  # I definitely could have put this in a multiple print statement but..
             print(countdown)
             countdown -= 1
-            # sleep(1)
+            sleep(1)
         print("Gooo!")
-
+        print("*Give it time to load if you're using iterative*")
         start_time = "HW"  # I have this in each if statement because if the user chooses quick_sort,
                            # the code will spend time moving to that last if statement and it won't be exact
+        arranged = 0
         if menu == 1:
             start_time = time()
             arranged = Bubble_Sort(numbers)
@@ -150,16 +153,20 @@ while menu != "EXIT":
 
         total_time = time() - start_time
         print("\nAnd TIME!!"
-              f"\nIt took {total_time: 8f} seconds for the method to finish sorting, (I only included 8 values after the decimal place).")
-
-        see = input("Would you like to see the sorted list? (Y or N)").title()
+              f"\nIt took {total_time} seconds for the method to finish sorting.")  # I had : .8f after to limit the decimals,
+                                                                                           # but it wouldn't show the correct value sometimes
+        see = input("Would you like to see the sorted list? (Y or N)"
+                    "\n>>>").title()
         if see == "Y" or see == "Yes":
             if menu2 >= 3000:
                 print("Once again, brace yourself:")
-            # sleep(1.5)
-            print(numbers)
-            input('press enter to continue'.upper())
+            sleep(1)
+            print(arranged)
+            input('click and press enter to continue'.upper())
 
         print('\nAlright, I\'m taking you back to the menu, where you can play around some more, or leave..'
               '\nBut you don\'t have to leave')
-        # sleep(.5)
+        numbers.clear()
+
+        input("\nclick here and Press Enter".upper())
+        break
